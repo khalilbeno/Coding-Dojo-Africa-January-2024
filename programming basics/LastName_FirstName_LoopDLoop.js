2a878f3f243f3d1dfdb2ac769d763f5eaeb73b4f
khalilbeno <kbenothman9@gmail.com>
2024-01-24 09:43:13 +0100

    predict

---
+++ programming basics/LastName_FirstName_LoopDLoop.js
@@ -0,0 +1,10 @@
+//we need a loop here coz the activity that the machine is repeted
+//the starting point of the loop is 0
+//the loop stop should stop at 6
+//number of miles will change after evrey loop
+//miles variable
+var miles
+for (var miles = 0; miles < 6; miles+=2) {
+  console.log(miles);
+  console.log("take a candy");
+}
