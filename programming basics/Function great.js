7422144ea9e48193ec4d5169387a3d028bed0391
khalilbeno <kbenothman9@gmail.com>
2024-01-24 13:01:51 +0100

    Function great

---
+++ programming basics/Function great.js
@@ -0,0 +1,26 @@
+function great(name){
+    console.log("great day "+name+"!");
+}
+great("Khalil");
+
+//============step2==========
+
+function great(name, time){
+    console.log("good "+" "+time+" "+name+"!");
+}
+great("Khalil", "afternoon");
+
+//============step3=========
+
+function great(name, time){
+    if (name== "Count Dooku" ){
+
+    console.log("I'm coming for you, Dooku!");
+    }
+    else{
+  
+    console.log("good"+" "+time+" " +name+"!");
+    } 
+}
+great("Khalil", "afternoon");
+great("Count Dooku")
