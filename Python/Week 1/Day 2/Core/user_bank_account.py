04aad23e866bb7fc3b774b88b590008aa805a89e
khalilbeno <kbenothman9@gmail.com>
2024-02-23 16:56:31 +0100

    python

--- Python/Week 1/Day 2/Core/user_bank_account.py
+++ Python/Week 1/Day 2/Core/user_bank_account.py
@@ -24,4 +24,20 @@ client2.deposit(100).deposit(100).withdraw(10).withdraw(10).withdraw(10).withdra
 
 
 
+class User:
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        self.account = BankAccount(int_rate=0.02, balance=0)
+    
+    # other methods
+    
+    def make_deposit(self, amount):
+        self.account.deposit(amount).display_account_info()
+
+    def make_withdrawal (self,amount):
+        self.account.withdraw(amount)
+
+    def  display_user_balance(self):
+        self.account.display_account_info()
 
