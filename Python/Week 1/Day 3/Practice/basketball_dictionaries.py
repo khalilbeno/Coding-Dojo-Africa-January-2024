3f6cc09d16047042f8ad602e1063270946ea545f
khalilbeno <kbenothman9@gmail.com>
2024-02-23 20:58:15 +0100

    python

---
+++ Python/Week 1/Day 3/Practice/basketball_dictionaries.py
@@ -0,0 +1,73 @@
+players = [
+    {
+        "name": "Kevin Durant",
+        "age": 34,
+        "position": "small forward",
+        "team": "Brooklyn Nets"
+    },
+    {
+        "name": "Jason Tatum",
+        "age": 24,
+        "position": "small forward",
+        "team": "Boston Celtics"
+    },
+    {
+        "name": "Kyrie Irving",
+        "age": 32,
+        "position": "Point Guard",
+        "team": "Brooklyn Nets"
+    },
+    {
+        "name": "Damian Lillard",
+        "age": 33,
+        "position": "Point Guard",
+        "team": "Portland Trailblazers"
+    },
+    {
+        "name": "Joel Embiid",
+        "age": 32,
+        "position": "Power Foward",
+        "team": "Philidelphia 76ers"
+    },
+    {
+        "name": "DeMar DeRozan",
+        "age": 32,
+        "position": "Shooting Guard",
+        "team": "Chicago Bulls"
+    }
+]
+
+class Player:
+    player_i = []
+    def __init__(self, player_info):
+        self.name = player_info["name"]
+        self.age = player_info["age"]
+        self.position = player_info["position"]
+        self.team = player_info["team"]
+        player_i.append(player_i)
+        player_i = Player(player_info)
+kevin = {
+    "name": "Kevin Durant",
+    "age": 34,
+    "position": "small forward",
+    "team": "Brooklyn Nets"
+}
+
+jason = {
+    "name": "Jason Tatum",
+    "age": 24,
+    "position": "small forward",
+    "team": "Boston Celtics"
+}
+
+kyrie = {
+    "name": "Kyrie Irving",
+    "age": 32,
+    "position": "Point Guard",
+    "team": "Brooklyn Nets"
+}
+
+kevin1 = Player(kevin)
+jason1 = Player(jason)
+kyrie1 = Player(kyrie)
+
