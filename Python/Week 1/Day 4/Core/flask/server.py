646862712bcc39854e4edf4859caeba528cbf3c2
khalilbeno <kbenothman9@gmail.com>
2024-02-26 13:35:30 +0100

    py

--- Python/Week 1/Day 4/Core/flask/server.py
+++ Python/Week 1/Day 4/Core/flask/server.py
@@ -7,7 +7,10 @@ def play():
 def play_num(num):
 
     return render_template("index1.html",num=num)
+@app.route('/play/<int:num>/<string:color>')
+def play_(num,color):
 
+    return render_template("index1.html",num=num,color=color)
 
 if __name__=="__main__":
     app.run(debug=True)
