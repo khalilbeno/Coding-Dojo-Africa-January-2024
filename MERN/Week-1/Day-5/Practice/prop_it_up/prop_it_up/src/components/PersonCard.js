fc7218a24c3f77546e476157ba8f6dfd7c293602
Khalil Ben Othman <kbenothman9@gmail.com>
2024-04-05 14:02:52 +0100

    prop_it_up day 5

--- MERN/Week-1/Day-5/Practice/prop_it_up/prop_it_up/src/components/PersonCard.js
+++ MERN/Week-1/Day-5/Practice/prop_it_up/prop_it_up/src/components/PersonCard.js
@@ -3,9 +3,9 @@ import react from 'react'
 const PersonCard = props => {
     return (
         <div>
-            <h2>{props.firstname} {props.lastname}</h2>
+            <h2>{props.firstname}, {props.lastname}</h2>
             <p>Age:{props.age}</p>
-            <p>Hair: {props.haircolor}</p>
+            <p>Hair color: {props.haircolor}</p>
         </div>
 
     )
