95ff8e58aa8485e621ad9d0ef04c22cdc58d0442
Khalil Ben Othman <kbenothman9@gmail.com>
2024-04-05 12:43:07 +0100

    FIRST_jsx day 4

---
+++ MERN/Week-1/Day-4/Core/first_jsx/first-jsx/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <setfield>
+      <div>
+        <h1>Hello Dojo </h1>
+        <h5>Things i need to do:</h5>
+        <ul>
+          <li>Learn React</li>
+          <li>Climb Mt.Everest</li>
+          <li>Run a marathon</li>
+          <li>Feed the dogs</li>
+        </ul>
+      </div>
+    </setfield>
+
+
+
+  );
+}
+
+export default App;
