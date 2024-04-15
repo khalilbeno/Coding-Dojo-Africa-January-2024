844cbff3073db7e2efb166263e2a07124513b936
Khalil Ben Othman <kbenothman9@gmail.com>
2024-04-15 02:37:43 +0100

    booox day 5

---
+++ MERN/Week-1/Day-5/Practice/box/box/src/components/box.js
@@ -0,0 +1,24 @@
+
+import React from 'react';
+
+const DisplayBox = (props) => {
+    const { boxcolorArray } = props;
+    return (
+        <div>
+            {boxcolorArray.map((box, index) => (
+                <div key={index}
+                    style={{
+                        backgroundColor: box.color,
+                        height: box.size,
+                        width: box.size,
+                        display: "inline-block",
+                        margin: "15px",
+                    }}>
+                </div>
+            ))
+            }
+        </div>
+    );
+}
+
+export default DisplayBox;
