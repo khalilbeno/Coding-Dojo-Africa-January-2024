c154bcf4ffb3c0e055b028e7873062ae262c28c0
Khalil Ben Othman <kbenothman9@gmail.com>
2024-05-13 16:49:01 +0100

    First Java practice day1

---
+++ Java/Week-1/Day1/Practice/First.java
@@ -0,0 +1,7 @@
+public class First {
+    public static void main(String[] args) {
+        System.out.println("My name is khalil Ben othman");
+        System.out.println("I am 28 years old");
+        System.out.println("My home town is in Nabeul");
+    }
+}
