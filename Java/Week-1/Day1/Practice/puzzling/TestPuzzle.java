825daa7b996f6b7629ec50d37556360303891a0d
Khalil Ben Othman <kbenothman9@gmail.com>
2024-05-18 00:31:41 +0100

    puzzle practice day1

---
+++ Java/Week-1/Day1/Practice/puzzling/TestPuzzle.java
@@ -0,0 +1,9 @@
+public class TestPuzzle {
+    public static void main(String[] args) {
+        PuzzleJava generator = new PuzzleJava();
+        System.out.println(generator.getTenRolls());
+        System.out.println(generator.getRandomLetter());
+        System.out.println(generator.generatePassword());
+        System.out.println(generator.generatePasswordSet(4));
+    }
+}
