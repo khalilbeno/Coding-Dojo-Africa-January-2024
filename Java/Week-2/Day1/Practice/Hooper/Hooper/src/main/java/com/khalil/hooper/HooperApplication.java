651803352201cd72588aedcd264d2620d56e4b0d
Khalil Ben Othman <kbenothman9@gmail.com>
2024-05-25 00:51:46 +0100

    hooper practice day1

---
+++ Java/Week-2/Day1/Practice/Hooper/Hooper/src/main/java/com/khalil/hooper/HooperApplication.java
@@ -0,0 +1,13 @@
+package com.khalil.hooper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HooperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HooperApplication.class, args);
+	}
+
+}
